a3281765fb0e9a1fadd80242875c7887c4b5f480
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-03-05 06:25:59 -0500

    Sign Modal is there

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 
-export default function Header(props) {
-  return (
-    <header {...props}>
-      <a className="logo-wrapper" href="https://antv.alipay.com/zh-cn/index.html" target="_blank">
-        <i className="logo" />
-        <span>Repap</span>
-      </a>
-      <div className="button">
-        <Button>Sign in</Button>
-      </div>
-    </header>
-  );
+export default class Header extends React.Component {
+
+  state = {
+    signInModalVisible: false
+  };
+
+  openSignInModal = (e) => {
+    this.setState({
+      signInModalVisible: !this.state.signInModalVisible
+    });
+  }
+
+  render() {
+    return (
+      <header {...this.props}>
+        <a className="logo-wrapper" href="https://antv.alipay.com/zh-cn/index.html" target="_blank">
+          <i className="logo" />
+          <span>Repap</span>
+        </a>
+        <div className="button">
+          <Button onClick={this.openSignInModal}>Sign in</Button>
+        </div>
+        <Modal
+          title="Sign In"
+          centered
+          visible={this.state.signInModalVisible}
+          footer={null}
+          onCancel={this.openSignInModal}
+        >
+          <p>some contents...</p>
+          <p>some contents...</p>
+          <p>some contents...</p>
+        </Modal>
+      </header>
+    );
+  }
+
 }
