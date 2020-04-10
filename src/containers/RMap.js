9f70a4c0ba1398225ede55de265955922eaf3645
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-04-10 23:01:29 +0100

    Input search added

--- src/containers/RMap.js
+++ src/containers/RMap.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { message, Tooltip, Alert, Button, Modal, Form, Input, Checkbox, Menu, Dropdown } from 'antd';
+import { message, Tooltip, Alert, Button, Modal, Form, Input, Checkbox, Menu, Dropdown, Col } from 'antd';
 
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import { Fab } from 'react-tiny-fab';
 import 'react-tiny-fab/dist/styles.css';
 
-const Search = Input;
+const { Search } = Input;
 
 class RMap extends React.Component {
+
+  onSearch = (value) => {
+    console.log('[onSearch]', value);
+  }
 
   render() {
 
     return (
-      <div>
-        <Search placeholder="input search loading with enterButton" loading enterButton />
-        <Map center={[3.844119, 11.501346]} zoom={12}>
+      <div className="RMap">
+        <Col xs={24} md={8} className="search">
+          <Search placeholder="Search a hotel" onSearch={this.onSearch} enterButton />
+        </Col>
+        <Map 
+          center={[3.844119, 11.501346]} 
+          zoom={16} 
+          zoomControl={false}
+        >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
