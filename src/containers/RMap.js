8ff40c8781970e146ed48b6900287eda1e4402c7
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-04-11 05:54:36 +0100

    Form for adding an hotel

--- src/containers/RMap.js
+++ src/containers/RMap.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Input, Typography, Col } from 'antd';
-
+import { Input, Typography, Col, Form, InputNumber, Button } from 'antd';
 import { Map, Marker, Popup, TileLayer, ZoomControl } from "react-leaflet";
 import { control } from "leaflet";
 import { Fab } from 'react-tiny-fab';
@@ -15,6 +14,23 @@ const { Text } = Typography;
 const center = [3.844119, 11.501346];
 
 class RMap extends React.Component {
+
+  state = {
+    isCreatingHotel: false
+  };
+
+  layout = {
+    labelCol: {
+      xs: { span: 12 },
+      sm: { span: 12 },
+      md: { span: 12 }
+    },
+    wrapperCol: {
+      xs: { span: 12 },
+      sm: { span: 8 },
+      md: { span: 12 }
+    }
+  }
   
   onSearch = (value) => {
     console.log('[onSearch]', value);
@@ -24,14 +40,70 @@ class RMap extends React.Component {
     console.log(map);
   }
 
+  onHotelCreationClick = () => {
+    const { isCreatingHotel } = this.state;
+    console.log('[onHotelCreation]');
+    this.setState({
+      isCreatingHotel: !isCreatingHotel
+    });
+  }
+
+  renderMarkers = () => {
+    return hotels.map(hotel => (
+      <Marker key={hotel.id} position={hotel.geo.split(", ")}>
+        <Popup><Text strong>{hotel.name}</Text><br />{hotel.address}</Popup>
+      </Marker>
+    ))
+    // .push(
+    //   <Marker position={center}>
+    //     <Popup>The center is here!</Popup>
+    //   </Marker>
+    // )
+  }
+
+  renderForm = () => {
+    return (
+      <Form
+        labelAlign="left"
+        layout="vertical"
+        name="hotel-creation"
+      >
+        <Form.Item name="name" label="Name" required>
+          <Input />
+        </Form.Item>
+        <Form.Item name="phone" label="Phone" required>
+          <Input />
+        </Form.Item>
+        <Form.Item name="price" label="Price (min)" required>
+          <InputNumber />
+        </Form.Item>
+        <Form.Item name="address" label="Address" required>
+          <Input />
+        </Form.Item>
+        <Form.Item name="infos" label="Informations/Description">
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    )
+  }
+
   render() {
 
     return (
       <div className="RMap">
         <Col xs={24} md={8} className="search">
-          <Search placeholder="Search a hotel" onSearch={this.onSearch} enterButton />
-          <HotelList hotels={hotels} />
-        </Col>
+        { !this.state.isCreatingHotel ? (
+          <div>
+            <Search placeholder="Search a hotel" onSearch={this.onSearch} enterButton />
+            <HotelList hotels={hotels} />
+          </div>
+        ) : this.renderForm() }
+        </Col>        
         <Map 
           center={center} 
           zoom={12}
@@ -42,21 +114,13 @@ class RMap extends React.Component {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={center}>
-            <Popup>The center is here!</Popup>
-          </Marker>
-          {
-            hotels.map(hotel => (
-              <Marker key={hotel.id} position={hotel.geo.split(", ")}>
-                <Popup><Text strong>{hotel.name}</Text><br />{hotel.address}</Popup>
-              </Marker>
-            ))
-          }
+          
+          { !this.state.isCreatingHotel ? this.renderMarkers() : null }
         </Map>
         <Fab
           icon={<span>+</span>} 
           mainButtonStyles={{ backgroundColor: '#e74c3c' }}
-          onClick={() => alert('Here is the action of FAB.')}
+          onClick={this.onHotelCreationClick}
           event={null}
           children={false}
         ></Fab>
