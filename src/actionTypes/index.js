8221b3b7f7486393ae88f5452dcf159e0adcb21c
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-04-19 13:42:37 -0400

    Add an hotel: Action Type defined

--- src/actionTypes/index.js
+++ src/actionTypes/index.js
@@ -16,3 +16,9 @@ export const CHECK_TOKEN_FAILED = 'auth.check_token.failed';
 
 export const AUTHENTICATED = 'auth.authenticated';
 export const UNAUTHENTICATED = 'auth.unauthenticated';
+
+
+export const HOTEL_ADD = 'hotel.add';
+export const HOTEL_ADD_PENDING = 'hotel.add.pending';
+export const HOTEL_ADD_SUCCESS = 'hotel.add.success';
+export const HOTEL_ADD_FAILED = 'hotel.add.failure';
